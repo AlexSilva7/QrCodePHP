922ae127cad97a20dcac9cf72b070cf18184bbe5
Alex Silva <alexaraujo_rj@yahoo.com.br>
2021-11-22 18:14:37 -0300

    new commit

--- index.php
+++ index.php
@@ -9,13 +9,15 @@ use \App\Pix\Payload;
 use Mpdf\QrCode\QrCode;
 use Mpdf\QrCode\Output;
 
+
 //INSTANCIA PRINCIPAL DO PAYLOAD PIX
 $obPayload = (new Payload)->setPixKey('alexaraujo_rj@yahoo.com.br')
 						->setDescription('CartolaFC')
 						->setMerchantName('Alex Silva')
-						  ->setMerchantCity('RIO DE JANEIRO')
-						  ->setAmount(30.00)
-						  ->setTxid('AlexSilva');
+						->setMerchantCity('RIO DE JANEIRO')
+						->setAmount(30.00)
+						->setTxid('AlexSilva');
+						
 
 //CODIGO DE PAGAMENTO PIX
 $payloadQrCode = $obPayload->getPayload();
@@ -23,13 +25,20 @@ $payloadQrCode = $obPayload->getPayload();
 //QR CODE
 $obQrCode = new QrCode($payloadQrCode);
 
+/*
+$testGD = get_extension_funcs("gd"); // Grab function list 
+if (!$testGD){ echo "GD not even installed.";
+	phpinfo();
+	 exit; }
+echo"<pre>".print_r($testGD,true)."</pre>";
+*/
 
-$image = (new Output\png)->output($obQrCode,400);
+$image = (new Output\Png)->output($obQrCode, 200);
 
+/*
 header('Content-Type: image/png');
 echo $image;
-
-
+*/
 /*
 echo "<pre>";
 print_r($obQrCode);
@@ -38,11 +47,12 @@ exit;
 */
 
 
-/*
-<!--
+?>
+
+
 <img src="data:image/png;base64, <?=base64_encode($image)?>">
+<br>
 <strong>
 	<?=$payloadQrCode?>
 </strong>
--->
-*/
+
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,15 +1,24 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '9e6571e088b89b3025f99092c56ce29a9b2d5d3d',
         'name' => 'wdev/pix',
         'dev' => true,
     ),
     'versions' => array(
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v201',
+            'version' => '201.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => 'aa5b19de5c45a32af27c87d655f5229b155fce25',
+            'dev_requirement' => true,
+        ),
         'mpdf/qrcode' => array(
             'pretty_version' => 'v1.1.1',
             'version' => '1.1.1.0',
@@ -29,12 +38,12 @@
             'dev_requirement' => false,
         ),
         'wdev/pix' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '9e6571e088b89b3025f99092c56ce29a9b2d5d3d',
             'dev_requirement' => false,
         ),
     ),
