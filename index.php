296e5e08de8b178b43169d635c7d485e2795e818
Alex Silva <alexaraujo_rj@yahoo.com.br>
2021-11-22 16:31:26 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<?php 
+
+//composer require mpdf/qrcode
+require __DIR__.'/vendor/autoload.php';
+
+//require 'vendor/autoload.php';
+
+//require "app/Pix/Payload.php";
+use \App\Pix\Payload;
+use Mpdf\QrCode\QrCode;
+use Mpdf\QrCode\Output;
+
+
+
+//INSTANCIA PRINCIPAL DO PAYLOAD PIX
+$obPayload = (new Payload)->setPixKey('alexaraujo_rj@yahoo.com.br')
+						->setDescription('CartolaFC')
+						->setMerchantName('Alex Silva')
+						  ->setMerchantCity('RIO DE JANEIRO')
+						  ->setAmount(30.00)
+						  ->setTxid('AlexSilva');
+
+//CODIGO DE PAGAMENTO PIX
+$payloadQrCode = $obPayload->getPayload();
+
+//QR CODE
+$obQrCode = new QrCode($payloadQrCode);
+
+$image = (new Output\Png)->output($obQrCode,400);
+
+header('Content-Type: image/png');
+echo $image;
+
+/*
+echo "<pre>";
+print_r($payloadQrCode);
+echo "</pre>";
+exit;
+*/
+
+/*
+<!--
+<img src="data:image/png;base64, <?=base64_encode($image)?>">
+<strong>
+	<?=$payloadQrCode?>
+</strong>
+-->
+*/
